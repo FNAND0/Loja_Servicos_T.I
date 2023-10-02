bf3414acd0dbbd2d8cc893451141f6ff721494bd
fernando <nandoviros09@gmail.com>
2023-10-02 14:50:00 -0300

    correções de pastas

--- cadastro_cliente.php
+++ cadastro_cliente.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="styles/styles_cadastro_cliente.css">
+        <link rel="stylesheet" href="css/styles_cadastro_cliente.css">
         <title>Service - T.I</title>
     </head>
     <body>
